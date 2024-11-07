0e67c165da0b9abd2b2d32f9985c9d1805c4afe2
Grunt-prog <86661317+Grunt-prog@users.noreply.github.com>
2024-11-08 00:35:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Ritesh123456"
+    return "Hello, Ritesh1234567"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
