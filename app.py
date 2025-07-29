07b06534bafdb81225048ce52e1a14d22bd187e7
Grunt-prog <86661317+Grunt-prog@users.noreply.github.com>
2025-07-29 15:34:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask, request
-import azure.functions as func
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def home():
     return "Hello, ritesh123"
-
-# Azure Functions Entry Point
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    """Azure Function entry point"""
-    return func.WsgiMiddleware(app.wsgi_app).handle(req)
